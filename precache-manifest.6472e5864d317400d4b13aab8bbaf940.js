7eca7cc97e91e50dc8312986558cf70da26d2aa4
Alexiss Diaz <alexiss.diaz@live.com>
2020-02-13 20:35:49 -0600

    Updates

---
+++ precache-manifest.6472e5864d317400d4b13aab8bbaf940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81abd0a633aa376cb1c26bb664049050",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a513203a928f3e107098",
+    "url": "/clicky-game/static/css/main.8b230213.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a513203a928f3e107098",
+    "url": "/clicky-game/static/js/main.f5315005.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
